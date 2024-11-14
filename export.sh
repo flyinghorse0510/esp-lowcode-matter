1c5ab6e363708016572da077ddce3ef898b98d73
Chirag Atal <chirag.atal@espressif.com>
2024-11-14 11:00:13 +0530

    LowCode sync: 2024-11-14

--- export.sh
+++ export.sh
@@ -2,36 +2,60 @@
 
 set -e
 
-# Function to log messages with color
-log_message() {
-    echo -e "\e[${2:-32}m$1\e[0m"
-}
-
 # Function to handle errors
 handle_error() {
-    log_message "Error: $1" 31
+    echo "Error: $1"
     exit 1
 }
 
+if [ -z "${LOW_CODE_PATH}" ]
+then
+    # LOW_CODE_PATH not set in the environment.
+    # If using bash or zsh, try to guess LOW_CODE_PATH from script location.
+    self_path=""
+
+    # shellcheck disable=SC2128  # ignore array expansion warning
+    if [ -n "${BASH_SOURCE-}" ]
+    then
+        self_path="${BASH_SOURCE}"
+    elif [ -n "${ZSH_VERSION-}" ]
+    then
+        self_path="${(%):-%x}"
+    else
+        echo "Could not detect LOW_CODE_PATH. Please set it before sourcing this script:"
+        echo "  export LOW_CODE_PATH=(add path here)"
+        return 1
+    fi
+
+    # shellcheck disable=SC2169,SC2169,SC2039  # unreachable with 'dash'
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # convert possibly relative path to absolute
+        script_dir="$(realpath_int "${self_path}")"
+        # resolve any ../ references to make the path shorter
+        script_dir="$(cd "${script_dir}" || exit 1; pwd)"
+    else
+        # convert to full path and get the directory name of that
+        script_name="$(readlink -f "${self_path}")"
+        script_dir="$(dirname "${script_name}")"
+    fi
+    export LOW_CODE_PATH="${script_dir}"
+    echo "Setting LOW_CODE_PATH to '${LOW_CODE_PATH}'"
+fi
+
 # Simplify directory names by creating variables
-ESP_MATTER_DIR="$(dirname "$0")/tools/dependencies/esp-matter"
+ESP_MATTER_DIR="${LOW_CODE_PATH}/tools/dependencies/esp-matter"
 ZAP_DIR="$ESP_MATTER_DIR/connectedhomeip/connectedhomeip/.environment/cipd/packages/zap"
 
 if [ -f "$ZAP_DIR/zap-cli"* ]; then
-  log_message "zap is already installed." 32
-  export ZAP_INSTALL_PATH=$(readlink -f "$ZAP_DIR")
-
+    echo "zap is already installed."
+    export ZAP_INSTALL_PATH="$ZAP_DIR"
 else
-  handle_error "zap is not installed. run install.sh first."
-  exit 1
+    handle_error "zap is not installed. run install.sh first."
 fi
 
 if [ ! -d "$ESP_MATTER_DIR" ]; then
-  handle_error "$ESP_MATTER_DIR folder does not exist."
-  exit 1
+    handle_error "$ESP_MATTER_DIR folder does not exist."
 fi
-export ESP_MATTER_PATH=$(readlink -f "$ESP_MATTER_DIR")
+export ESP_MATTER_PATH="$ESP_MATTER_DIR"
 
-export LOW_CODE_PATH=$(readlink -f $(dirname $0))
-
-log_message "Successfully exported variables" 32
+echo "Successfully exported variables"
--- install.sh
+++ install.sh
@@ -2,14 +2,9 @@
 
 set -e
 
-# Function to log messages with color
-log_message() {
-    echo -e "\e[${2:-32}m$1\e[0m"
-}
-
 # Function to handle errors
 handle_error() {
-    log_message "Error: $1" 31
+    echo "Error: $1"
     exit 1
 }
 
@@ -34,23 +29,57 @@ case $OS in
     *) handle_error "Unsupported OS" ;;
 esac
 
+if [ -z "${LOW_CODE_PATH}" ]
+then
+    # LOW_CODE_PATH not set in the environment.
+    # If using bash or zsh, try to guess LOW_CODE_PATH from script location.
+    self_path=""
+
+    # shellcheck disable=SC2128  # ignore array expansion warning
+    if [ -n "${BASH_SOURCE-}" ]
+    then
+        self_path="${BASH_SOURCE}"
+    elif [ -n "${ZSH_VERSION-}" ]
+    then
+        self_path="${(%):-%x}"
+    else
+        echo "Could not detect LOW_CODE_PATH. Please set it before sourcing this script:"
+        echo "  export LOW_CODE_PATH=(add path here)"
+        return 1
+    fi
+
+    # shellcheck disable=SC2169,SC2169,SC2039  # unreachable with 'dash'
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # convert possibly relative path to absolute
+        script_dir="$(realpath_int "${self_path}")"
+        # resolve any ../ references to make the path shorter
+        script_dir="$(cd "${script_dir}" || exit 1; pwd)"
+    else
+        # convert to full path and get the directory name of that
+        script_name="$(readlink -f "${self_path}")"
+        script_dir="$(dirname "${script_name}")"
+    fi
+    export LOW_CODE_PATH="${script_dir}"
+    echo "Setting LOW_CODE_PATH to '${LOW_CODE_PATH}'"
+fi
+
 # Simplify directory names by creating a variable
-INSTALL_DIR="$(dirname "$0")/tools/dependencies/esp-matter/connectedhomeip/connectedhomeip/.environment/cipd/packages"
+INSTALL_DIR="${LOW_CODE_PATH}/tools/dependencies/esp-matter/connectedhomeip/connectedhomeip/.environment/cipd/packages"
 
 mkdir -p "$INSTALL_DIR" || handle_error "Failed to create directory $INSTALL_DIR"
-log_message "Installing zap..." 32
+echo "Installing zap..."
 
 curl -L -o "$INSTALL_DIR/zap.zip" $URL || handle_error "Failed to download zap.zip"
 
 if unzip -q "$INSTALL_DIR/zap.zip" -d "$INSTALL_DIR/zap"; then
-    log_message "Unzip successful." 32
+    echo "Unzip successful."
 else
     handle_error "Unzip failed"
 fi
 if [ -f "$INSTALL_DIR/zap/zap-cli"* ]; then
-  rm "$INSTALL_DIR/zap.zip"
-  log_message "Successfully Downloaded zap" 32
+    rm "$INSTALL_DIR/zap.zip"
+    echo "Successfully Downloaded zap"
 else
-  handle_error "Issue in unzipping the file"
+    handle_error "Issue in unzipping the file"
 fi
 
